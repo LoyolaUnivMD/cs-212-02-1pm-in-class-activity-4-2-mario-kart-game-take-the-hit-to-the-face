a363eec036fbfd88e891bd7e06846f52f6824f81
Gigi <gli@loyola.edu>
2024-02-07 19:46:17 -0500

    did some of the code

--- marioKart.java
+++ marioKart.java
@@ -3,10 +3,48 @@
 // Your code here
 // Delete these 4 lines of comments
 
-class HelloWorld {
-    public static void main(String[] args) {
+public class MarioKartPlayer{ //start of the player class
+    private String playerName;
+    private String character;
+    private int speed;
 
-        System.out.println("Mario Kart!");
-        
+    //General Constructor (parameterized)
+    public MarioKartPlayer(String playerName, String character, int speed) {
+        this.playerName = playerName;
+        this.character = character;
+        this.speed = speed;
     }
+
+    //Default Constructor with default values
+    public MarioKartPlayer(){
+        this.playerName = "player";
+        this.character = "unknown";
+        this.speed = 0;
+    }
+
+    //Boosting speed method
+    public int boost(){
+        speed += 1;
+        return speed;
+    }
+
+    //Displaying info method
+    public String displayInfo(){
+        System.out.println("Hi " + playerName + ", you will be playing " + character + ". Your current speed is " + speed + ".");
+        return null;
+    }
+
+    //Princess Peach object
+    public static void main(String[] args) {
+        MarioKartPlayer princessPeach = new MarioKartPlayer("Debbie", "Princess Peach", 3);
+
+    }
+
+    //Luigi object
+    public static void main(String[] args) {
+        MarioKartPlayer princessPeach = new MarioKartPlayer("Vanessa", "Luigi", 4);
+
+    }
+
+
 }
