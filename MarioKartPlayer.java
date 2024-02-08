8134243fd2afe22f3731489e5ea5ca32b2e2fbdf
Gigi <gli@loyola.edu>
2024-02-08 02:44:32 -0500

    finalized code with comments

--- MarioKartPlayer.java
+++ MarioKartPlayer.java
@@ -1,9 +1,16 @@
-// Read the README.md
-// Read it again
-// Your code here
-// Delete these 4 lines of comments
+// MarioKartPlayer.java
+// Programmer: Gigi Li
+// Course:  CS 212, Mr. John
+// Due Date: 2/8/23
+// In-Class Assignment: Mario Kart Game
+// Problem Statement:
+// Inputs:
+// Output:
+// Credits: none
+
 
 public class MarioKartPlayer{ //start of the player class
+    //Data fields
     private String playerName;
     private String character;
     private int speed;
@@ -34,17 +41,25 @@ public class MarioKartPlayer{ //start of the player class
         return null;
     }
 
-    //Princess Peach object
+    //Create main method for 2 characters
     public static void main(String[] args) {
+        //Princess Peach Object
         MarioKartPlayer princessPeach = new MarioKartPlayer("Debbie", "Princess Peach", 3);
+        princessPeach.displayInfo();
+        princessPeach.boost();
+        System.out.println("We have boosted " + princessPeach.character + "'s speed!");
+        System.out.println(princessPeach.character + " now has " + princessPeach.speed + " speed.");
+        System.out.println();
 
-    }
+        //Luigi Object
+        MarioKartPlayer luigi = new MarioKartPlayer("Vanessa", "Luigi", 4);
+        luigi.displayInfo();
+        luigi.boost();
+        System.out.println("We have boosted " + luigi.character + "'s speed!");
+        System.out.println(luigi.character + " now has " + luigi.speed + " speed.");
+        System.out.println();
 
-    //Luigi object
-    public static void main(String[] args) {
-        MarioKartPlayer princessPeach = new MarioKartPlayer("Vanessa", "Luigi", 4);
-
-    }
+    }//End of main method
 
 
-}
+}//end of MarioKartPlayer class
